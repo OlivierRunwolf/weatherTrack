aed59e7fe0900351fef0f0ba3a0ffc54fe203054
Olivier <olivier@renouf.com>
2022-01-25 22:10:02 -0500

    writing to a csv, why you ask? why not

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # This is a sample Python script.
-import python_weather, asyncio, schedule, time
+import python_weather, asyncio, schedule, time, json
 from tinydb import TinyDB, Query
+import pandas as pd
+from io import StringIO
+
 # exec(open('main.py').read())
 print('''
  __          ________       _______ _    _ ______ _____                       
@@ -17,33 +20,51 @@ print('''
 ''')
 print("Running script....")
 
+
 async def getweather():
     db = TinyDB('db.json')
     client = python_weather.Client(format=python_weather.METRIC)
 
     weather = await client.find("Montreal Quebec")
 
-  #  print(weather.current.temperature,"In montreal right now",sep=" // ")
+    #  print(weather.current.temperature,"In montreal right now",sep=" // ")
 
     for forecast in weather.forecasts:
         Duplicata = Query()
         if not db.search(Duplicata.date == str(forecast.date)):
-            print(forecast.date,"date not added. addings to database",sep=" ")
-            db.insert({'date': str(forecast.date), 'temperature': forecast.temperature, 'condiiton': str(forecast.sky_text)})
+            print(forecast.date, "date not added. addings to database", sep=" ")
+            db.insert(
+                {'date': str(forecast.date), 'temperature': forecast.temperature, 'condiiton': str(forecast.sky_text)})
             print(str(forecast.date), forecast.sky_text, forecast.temperature)
         else:
             print("data already added")
 
+
+    # TODO: review this whold ordeal down here
+    r = open('db.json')
+    data = json.load(r)
+    dataob = StringIO(json.dumps(data['_default']))
+    print(json.dumps(data['_default']))
+    print(type(data))
+    pandareader = pd.read_json(dataob, typ='frame', orient='index')
+    pandareader.to_csv('out.csv')
+    r.close()
     await client.close()
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(getweather())
+
 
 def job():
     loop = asyncio.get_event_loop()
     loop.run_until_complete(getweather())
 
-schedule.every(10).minutes.do(job)
+# schedule.every(10).minutes.do(job)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
